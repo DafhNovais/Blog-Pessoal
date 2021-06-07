ee5c642e3aba0ff6d7be8c7d33057813ccc27b10
Dafh.Novais <dafhnenovais@gmail.com>
2021-06-07 17:20:08 -0300

    Segurança Back-And

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/seguranca/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package org.generation.blogPessoal.seguranca;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.generation.blogPessoal.model.Usuario;
 import org.springframework.security.core.GrantedAuthority;
@@ -9,20 +10,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class UserDetailsImpl implements UserDetails{
 
 	private static final long serialVersionUID = 1L;
-	private String userName;
-	private String password;
-		
-	public UserDetailsImpl(Usuario user) {
-		this.userName = user.getUsuario();
-		this.password = user.getSenha();
-	}
-	
-	public UserDetailsImpl () {}
+    private String userName;
+    private String password;
+    private List<GrantedAuthority> authorities;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+    public UserDetailsImpl(Usuario user) {
+        this.userName = user.getUsuario();
+        this.password = user.getSenha();
+
+    }
+
+    public UserDetailsImpl () {}
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
+        return authorities;
 	}
 
 	@Override
